c81ba069f5f7295ff36ed33de8054fdb1a1d6eec
Dylan <dylan.djg8413@gmail.com>
2026-03-19 12:59:07 -0500

    Implementa FakeTaskRepository con inyección de dependencias

--- app/src/main/java/com/tuusuario/myactivity/data/FakeTaskRepository.kt
+++ app/src/main/java/com/tuusuario/myactivity/data/FakeTaskRepository.kt
@@ -10,9 +10,11 @@ class FakeTaskRepository @Inject constructor() : TaskRepository {
         Task(2, "Implementar ViewModel con StateFlow en Android"),
         Task(3, "Configurar Hilt en el proyecto con KSP"),
         Task(4, "Escribir test unitario del ViewModel", completed = true),
+
     )
     override suspend fun getAllTasks(): List<Task> = tasks.toList()
     override suspend fun addTask(title: String) {
         tasks.add(Task(id = tasks.size.toLong() + 1, title = title))
     }
+
 }
